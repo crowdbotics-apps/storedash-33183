5b0568af11c4c3c61ae9516e58a17bd4359ee8f1
Crowdbotics <team@crowdbotics.com>
2022-01-26 14:05:47 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/storedash_33183/MainActivity.java
@@ -0,0 +1,15 @@
+package com.storedash_33183;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "storedash_33183";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "storedash_33183",
+  url: "https://storedash_33183.botics.co",
+  api: "https://storedash_33183.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
